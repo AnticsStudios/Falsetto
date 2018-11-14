27ea219b56e06e1b91a47291b0f2d091f83221bb
SLCTECH\ddolan <quyon_danny@hotmail.com>
2018-11-14 16:56:32 -0500

    Worked on making the random walls pop up

--- 7WeekGame/Assets/Scripts/SoundManager.cs
+++ 7WeekGame/Assets/Scripts/SoundManager.cs
@@ -1,20 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-public AudioSource efxSource;                   //Drag a reference to the audio source which will play the sound effects.
-public AudioSource musicSource;                 //Drag a reference to the audio source which will play the music.
-public static SoundManager instance = null;     //Allows other scripts to call functions from SoundManager.             
-public float Pitch1 = .95f;              //The lowest a sound effect will be randomly pitched.
-public float highPitchRange = 1.05f;            //The highest a sound effect will be randomly pitched.
-public class SoundManager : MonoBehaviour {
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//public AudioSource efxSource;                   //Drag a reference to the audio source which will play the sound effects.
+//public AudioSource musicSource;                 //Drag a reference to the audio source which will play the music.
+//public static SoundManager instance = null;     //Allows other scripts to call functions from SoundManager.             
+//public float Pitch1 = .95f;              //The lowest a sound effect will be randomly pitched.
+//public float highPitchRange = 1.05f;            //The highest a sound effect will be randomly pitched.
+//public class SoundManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+//	// Use this for initialization
+//	void Start () {
 		
-	}
+//	}
 	
-	// Update is called once per frame
-	void Update () {
+//	// Update is called once per frame
+//	void Update () {
 		
-	}
-}
+//	}
+//}
