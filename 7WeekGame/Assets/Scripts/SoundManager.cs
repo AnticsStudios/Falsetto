a4d2c473e9003dfd60c53c288dd858d804a7da12
SLCTECH\emarleau <eryckmarleau@hotmail.com>
2018-11-14 16:22:11 -0500

    SCRIPT SHOULD HOPEFULLY WORK

--- 7WeekGame/Assets/Scripts/SoundManager.cs
+++ 7WeekGame/Assets/Scripts/SoundManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    public AudioSource efxSource;                   
+    public AudioSource musicSource;                 
     public void PlaySingle(AudioClip clip)
     {
         //Set the clip of our efxSource audio source to the clip passed in as a parameter.
