9ffe299e71db68c0aebd6e60c1f7cb1c5ea4c7df
SLCTECH\emarleau <eryckmarleau@hotmail.com>
2018-11-21 16:18:07 -0500

    SoundManager Complete

--- 7WeekGame/Assets/Scripts/SoundManager.cs
+++ 7WeekGame/Assets/Scripts/SoundManager.cs
@@ -1,20 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//public AudioSource efxSource;                   //Drag a reference to the audio source which will play the sound effects.
-//public AudioSource musicSource;                 //Drag a reference to the audio source which will play the music.
-//public static SoundManager instance = null;     //Allows other scripts to call functions from SoundManager.             
-//public float Pitch1 = .95f;              //The lowest a sound effect will be randomly pitched.
-//public float highPitchRange = 1.05f;            //The highest a sound effect will be randomly pitched.
-//public class SoundManager : MonoBehaviour {
 
-//	// Use this for initialization
-//	void Start () {
-		
-//	}
-	
-//	// Update is called once per frame
-//	void Update () {
-		
-//	}
-//}
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance = null;
+    //  Use this for initialization
+    public AudioSource EffectsSource;
+    public AudioSource MusicSource;
+    public AudioClip Death;
+    public AudioClip GlassCollision;
+    public AudioClip Attack1;
+    public AudioClip Attack2;
+    public AudioClip Attack3;
+    public AudioClip WallCollision;
+    public AudioClip Explosion;
+   
+
+    public void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+        public void PlaySingle(AudioClip clip)
+        {
+        EffectsSource.clip = clip;
+        EffectsSource.Play();
+        }
+        public void MusicBackground(AudioSource Background)
+        {
+        MusicSource.Play();
+        }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
