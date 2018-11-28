c77ca516620dfc8dd0a0bb01d99dca2b1503da54
SLCTECH\ddolan <quyon_danny@hotmail.com>
2018-11-28 17:21:43 -0500

    🏆🏆🏆🏆🏆

---
+++ 7WeekGame/Assets/Scripts/ButtonPress.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonPress: MonoBehaviour
+{
+
+    public AudioSource soundclip;
+    private Rect ButtonLocation;
+
+    void Start()
+    {
+       
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    void OnGUI()
+    {
+        
+
+      
+            if (Input.GetMouseButtonDown(0))
+            {
+                soundclip.Play();
+            }
+
+
+        }
+    }
+
