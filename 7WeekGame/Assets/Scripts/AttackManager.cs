6433ad0ce3c8d72ab4252fc50e3fccf2ff5ff6b9
anticsminus <antics_chris@hotmail.com>
2018-12-06 01:35:29 -0500

    fixed player attack script, made modular can now instantiate prefabse

--- 7WeekGame/Assets/Scripts/AttackManager.cs
+++ 7WeekGame/Assets/Scripts/AttackManager.cs
@@ -7,146 +7,145 @@ public class AttackManager : MonoBehaviour {
     public Animator projectile1;
     public Animator projectile2;
     public Animator projectile3;
+    public GameObject Charged1;
+    public GameObject Charged2;
+    public GameObject Charged3;
     public Transform ActualPlayer;
-    public SpriteRenderer Charged;
-    public SpriteRenderer Charged2;
-    public SpriteRenderer Charged3;
-
-    public float projectileSpeed = 0f;
+    public Transform Launcher;
+    public bool isAttacking = false;
+    public float projectileSpeed = 0.1f;
 
     // Use this for initialization
     void Start ()
     {
         anim = GetComponent<Animator>();
-
-        projectile1.enabled = false;
-        Charged.enabled = false;
-
-
-        projectile2.enabled = false;
-        Charged2.enabled = false;
-
-        projectile3.enabled = false;
-        Charged3.enabled = false;
-
-        projectile1.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
-        projectile2.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
-        projectile3.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+
+    GameObject FireBullets(GameObject name)
     {
+
+        GameObject ChargedUpShot = Instantiate(name) as GameObject;
+        return ChargedUpShot;
+
+    }
+
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if(Input.anyKey)
+        {
+            if (isAttacking == true)
+            {
+                if (gameObject.GetComponent<MovePlayer>().facingRight == false)
+                {
+                    gameObject.GetComponent<MovePlayer>().FlipPlayer();
+                    gameObject.GetComponent<MovePlayer>().enabled = false;
+                    gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+                    projectile1.SetBool("isIdle", false);
+                    anim.SetBool("Attack", true);
+
+                }
+                else
+                {
+                    gameObject.GetComponent<MovePlayer>().enabled = false;
+                    gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+                    projectile1.SetBool("isIdle", false);
+                    anim.SetBool("Attack", true);
+                }
+
+            }
+            else
+            {
+                projectile1.SetBool("isIdle", true);
+                anim.SetBool("Attack", false);
+                gameObject.GetComponent<MovePlayer>().enabled = true;
+
+            }
+        }
+
+        if (Input.anyKey != true)
+        {
+            anim.SetBool("Attack", false);
+            
+        }
+
         // projectile1.GetFloat("WeakAtk");
         if (Input.GetKeyDown(KeyCode.Keypad1))
         {
-            if (gameObject.GetComponent<MovePlayer>().facingRight == false)
-            {
-                gameObject.GetComponent<MovePlayer>().FlipPlayer();
-            }
-
-                projectile1.SetBool("isIdle", false);
-                anim.SetBool("Attack", true);
-                projectile1.enabled = true;
-                projectile1.Play("WeakAtk");
-                Charged.enabled = false;
-                gameObject.GetComponent<MovePlayer>().enabled = false;
-                gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
-                projectile1.transform.position = anim.transform.position + new Vector3(0.9f, 0.1f, 0.0f);
-  
-            
-
+            isAttacking = true;
+            projectile1.enabled = true;
+            projectile1.Play("WeakAtk");
+            projectile1.transform.position = anim.transform.position + new Vector3(0.9f, 0.1f, 0.0f);
         }
         if (Input.GetKeyUp(KeyCode.Keypad1))
         {
-            Charged.enabled = true;
-            anim.SetBool("Attack", false);
-            projectile1.speed.Equals(0.0f);
             projectile1.transform.position = new Vector3(-400.0f, 0.0f, 0);
-            projectileSpeed = 0.1f;
-            gameObject.GetComponent<MovePlayer>().facingRight = true;
-            gameObject.GetComponent<MovePlayer>().enabled = true;
-            Charged.transform.position = anim.transform.position + new Vector3(0.9f, 0.1f, 0);
-            projectile1.SetFloat("WeakAtk", 0);
-            projectile1.SetBool("isIdle", true); 
-
+            GameObject Charged = FireBullets(Charged1);
+            Charged.transform.position = Launcher.transform.position;
+            Charged.GetComponent<Rigidbody2D>().velocity = new Vector2(5, 0);
+            isAttacking = false;
         }
 
-        if (Input.GetKeyDown(KeyCode.Keypad2))
-        {
-            if (gameObject.GetComponent<MovePlayer>().facingRight == false)
-            {
-                gameObject.GetComponent<MovePlayer>().FlipPlayer();
-            }
-            projectile2.SetBool("isIdle", false);
-            anim.SetBool("Attack", true);
-            projectile2.enabled = true;
-            projectile2.Play("MediumAtk");
-            Charged2.enabled = false;
-            gameObject.GetComponent<MovePlayer>().facingRight = true;
-            gameObject.GetComponent<MovePlayer>().enabled = false;
-            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
-            projectile2.transform.position = anim.transform.position + new Vector3(0.9f, 0.1f, 0.0f);
-            if (projectile2.GetFloat("FixedTime") > 1)
-            {
-                projectile2.enabled = false;
+        //if (Input.GetKeyDown(KeyCode.Keypad2))
+        //{
 
-            }
+        //    projectile2.SetBool("isIdle", false);
+        //    anim.SetBool("Attack", true);
+        //    projectile2.enabled = true;
+        //    projectile2.Play("MediumAtk");
+        //    Charged2.enabled = false;
 
-        }
-        if (Input.GetKeyUp(KeyCode.Keypad2))
-        {
-            Charged2.enabled = true;
+        //    projectile2.transform.position = anim.transform.position + new Vector3(0.9f, 0.1f, 0.0f);
+        //    if (projectile2.GetFloat("FixedTime") > 1)
+        //    {
+        //        projectile2.enabled = false;
 
-            anim.SetBool("Attack", false);
-            projectile2.speed.Equals(0.0f);
-            projectile2.transform.position = new Vector3(-400.0f, 0.0f, 0);
-            projectileSpeed = 0.1f;
-            gameObject.GetComponent<MovePlayer>().enabled = true;
-            Charged2.transform.position = anim.transform.position + new Vector3(0.9f, 0.1f, 0);
-            projectile2.SetBool("isIdle", true);
-        }
+        //    }
+
+        //}
+        //if (Input.GetKeyUp(KeyCode.Keypad2))
+        //{
+        //    Charged2.enabled = true;
+
+        //    anim.SetBool("Attack", false);
+        //    projectile2.speed.Equals(0.0f);
+        //    projectile2.transform.position = new Vector3(-400.0f, 0.0f, 0);
+        //    projectileSpeed = 0.1f;
+        //    gameObject.GetComponent<MovePlayer>().enabled = true;
+        //    Charged2.transform.position = anim.transform.position + new Vector3(0.9f, 0.1f, 0);
+        //    projectile2.SetBool("isIdle", true);
+        //}
 
 
 
-        if (Input.GetKeyDown(KeyCode.Keypad3))
-        {
-            if (gameObject.GetComponent<MovePlayer>().facingRight == false)
-            {
-                gameObject.GetComponent<MovePlayer>().FlipPlayer();
-            }
-            projectile3.SetBool("isIdle", false);
-            anim.SetBool("Attack", true);
-            projectile3.enabled = true;
-            projectile3.Play("StrongAtk");
-            Charged3.enabled = false;
-            gameObject.GetComponent<MovePlayer>().facingRight = true;
-            gameObject.GetComponent<MovePlayer>().enabled = false;
-            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
-            projectile3.transform.position = anim.transform.position + new Vector3(0.9f, 0.1f, 0.0f);
+        //if (Input.GetKeyDown(KeyCode.Keypad3))
+        //{
 
-        }
-        if (Input.GetKeyUp(KeyCode.Keypad3))
-        {
-            Charged3.enabled = true;
+        //    projectile3.SetBool("isIdle", false);
+        //    anim.SetBool("Attack", true);
+        //    projectile3.enabled = true;
+        //    projectile3.Play("StrongAtk");
+        //    Charged3.enabled = false;
+        //    gameObject.GetComponent<MovePlayer>().facingRight = true;
+        //    gameObject.GetComponent<MovePlayer>().enabled = false;
+        //    gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        //    projectile3.transform.position = anim.transform.position + new Vector3(0.9f, 0.1f, 0.0f);
 
-            anim.SetBool("Attack", false);
-            projectile3.speed.Equals(0.0f);
-            projectile3.transform.position = new Vector3(-400.0f, 0.0f, 0);
-            projectileSpeed = 0.1f;
-            gameObject.GetComponent<MovePlayer>().enabled = true;
-            Charged3.transform.position = anim.transform.position + new Vector3(0.9f, 0.1f, 0);
-            projectile3.SetBool("isIdle", true);
-        }
+        //}
+        //if (Input.GetKeyUp(KeyCode.Keypad3))
+        //{
+
+        //}
 
 
 
 
-        Charged3.transform.position += new Vector3(projectileSpeed, 0, 0);
+        //Charged3.transform.position += new Vector3(projectileSpeed, 0, 0);
 
-        Charged2.transform.position += new Vector3(projectileSpeed, 0, 0);
+        //Charged2.transform.position += new Vector3(projectileSpeed, 0, 0);
 
-        Charged.transform.position += new Vector3(projectileSpeed, 0, 0);
     }
 
 
