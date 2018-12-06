c9e867df7454a650d93f546524942a1a0c8a153b
SLCTECH\clandry <antics_chris@hotmail.com>
2018-12-06 16:51:32 -0500

    fixed glass animations fixed attacks fixed bugs for attacks fixed collisions made modular

--- 7WeekGame/Assets/Scripts/GlassHitDecection.cs
+++ 7WeekGame/Assets/Scripts/GlassHitDecection.cs
@@ -7,6 +7,7 @@ public class GlassHitDecection : MonoBehaviour
 {
 
     public Animator anim;
+    public Animator glasses;
     public Animator[] strongGlass;
     public Animator[] mediumGlass;
     public Animator[] weakGlass;
@@ -20,10 +21,8 @@ public class GlassHitDecection : MonoBehaviour
     public GameObject[] oweakGlass;
 
 
-    public int glassAmount;
-
     void Start()
-     
+
     {
         Player = GameObject.FindGameObjectWithTag("Player");
 
@@ -36,19 +35,15 @@ public class GlassHitDecection : MonoBehaviour
         oweakGlass = GameObject.FindGameObjectsWithTag("Glass1");
         omediumGlass = GameObject.FindGameObjectsWithTag("Glass2");
         ostrongGlass = GameObject.FindGameObjectsWithTag("Glass3");
+
     }
 
-    public void doAnimations(GameObject[] animateGlass)
-    {
-        Animator glass = animateGlass[0].GetComponent<Animator>();
-        glass.SetBool("isIdle", false);
-        glass.GetComponent<BoxCollider2D>().enabled = false;
-        Debug.Log("in here");
-    }
 
-        void OnTriggerEnter2D(Collider2D other)   // If player hits Heavy glass they Die
+
+
+    void OnTriggerEnter2D(Collider2D other)   // If player hits Heavy glass they Die
     {
-        if (other.gameObject.tag == "Glass1"|| other.gameObject.tag == "Glass2"|| other.gameObject.tag == "Glass3")
+        if (other.gameObject.tag == "Glass1" || other.gameObject.tag == "Glass2" || other.gameObject.tag == "Glass3")
         {
             if (this.gameObject.tag == "Player")
             {
@@ -61,40 +56,43 @@ public class GlassHitDecection : MonoBehaviour
         {
             if (this.gameObject.tag == "WeakAtk")
             {
-                doAnimations(oweakGlass);
-                //GetComponent<Rigidbody2D>().gravityScale = 1;
+                Animator glass = other.GetComponent<Animator>();
+                glass.SetBool("isIdle", false);
+                Destroy(this.gameObject);
+
             }
-            else if((this.gameObject.tag == "MediumAtk" || this.gameObject.tag == "StrongAtk"))
+            else if ((this.gameObject.tag == "MediumAtk" || this.gameObject.tag == "StrongAtk"))
             {
                 Destroy(this.gameObject);
             }
         }
-            if (other.gameObject.tag == "Glass2")
+        if (other.gameObject.tag == "Glass2")
+        {
+            if (this.gameObject.tag == "MediumAtk")
             {
-                if (this.gameObject.tag == "MediumAtk")
-                {
-                doAnimations(omediumGlass);
-                Debug.Log("in here");
-
-                //GetComponent<Rigidbody2D>().gravityScale = 1;
-                 }
-                else if ((this.gameObject.tag == "WeakAtk" || this.gameObject.tag == "StrongAtk"))
-                {
-                    Destroy(this.gameObject);
-                }
+                Animator glass = other.GetComponent<Animator>();
+                glass.SetBool("isIdle", false);
+                Destroy(this.gameObject);
+            }
+            else if ((this.gameObject.tag == "WeakAtk" || this.gameObject.tag == "StrongAtk"))
+            {
+                Destroy(this.gameObject);
+            }
         }
-            if (other.gameObject.tag == "Glass3")
+        if (other.gameObject.tag == "Glass3")
+        {
+            if (this.gameObject.tag == "StrongAtk")
             {
-                if (this.gameObject.tag == "StrongAtk")
-                {
-                doAnimations(ostrongGlass);
+                Animator glass = other.GetComponent<Animator>();
+                glass.SetBool("isIdle", false);
+                Destroy(this.gameObject);
             }
             else if ((this.gameObject.tag == "MediumAtk" || this.gameObject.tag == "WeakAtk"))
             {
                 Destroy(this.gameObject);
             }
         }
-        
+
 
 
     }
---
+++ 7WeekGame/Assets/killGlass.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class killGlass : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    public void destroyGlass()
+    {
+        Destroy(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
