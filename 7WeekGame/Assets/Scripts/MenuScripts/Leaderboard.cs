1378e15882545ac45c95873fbfbf957fb569d481
Robert <Robertwilliamworld@hotmail.com>
2018-12-10 22:03:00 -0500

    LeaderBoard script

--- 7WeekGame/Assets/Scripts/MenuScripts/Leaderboard.cs
+++ 7WeekGame/Assets/Scripts/MenuScripts/Leaderboard.cs
@@ -1,80 +1,83 @@
-﻿using UnityEngine;
-using System.Collections.Generic;
+﻿//using UnityEngine;
+//using System.Collections.Generic;
 
-public class Leaderboard : MonoBehaviour
-{
-    public const int EntryCount = 10;
+//public class Leaderboard : MonoBehaviour
+//{
+//    public const int EntryCount = 10;
 
-    public struct ScoreEntry
-    {
-        public string name;
-        public int score;
 
-        public ScoreEntry(string name, int score)
-        {
-            this.name = name;
-            this.score = score;
-        }
-    }
+//    public struct ScoreEntry
+//    {
+//        public string name;
+//        public int score;
 
-    private static List<ScoreEntry> s_Entries;
+//        public ScoreEntry(string name, int score)
+//        {
+//            this.name = name;
+//            this.score = score;
+//        }
+//    }
 
-    private static List<ScoreEntry> Entries
-    {
-        get
-        {
-            if (s_Entries == null)
-            {
-                s_Entries = new List<ScoreEntry>();
-                LoadScores();
-            }
-            return s_Entries;
-        }
-    }
+    
 
-    private const string PlayerPrefsBaseKey = "leaderboard";
+//    private static List<ScoreEntry> s_Entries;
 
-    private static void SortScores()
-    {
-        s_Entries.Sort((a, b) => b.score.CompareTo(a.score));
-    }
+//    private static List<ScoreEntry> Entries
+//    {
+//        get
+//        {
+//            if (s_Entries == null)
+//            {
+//                s_Entries = new List<ScoreEntry>();
+//                LoadScores();
+//            }
+//            return s_Entries;
+//        }
+//    }
 
-    private static void LoadScores()
-    {
-        s_Entries.Clear();
+//    private const string PlayerPrefsBaseKey = "leaderboard";
 
-        for (int i = 0; i < EntryCount; ++i)
-        {
-            ScoreEntry entry;
-            entry.name = PlayerPrefs.GetString(PlayerPrefsBaseKey + "[" + i + "].name", "");
-            entry.score = PlayerPrefs.GetInt(PlayerPrefsBaseKey + "[" + i + "].score", 0);
-            s_Entries.Add(entry);
-        }
+//    private static void SortScores()
+//    {
+//        s_Entries.Sort((a, b) => b.score.CompareTo(a.score));
+//    }
 
-        SortScores();
-    }
+//    private static void LoadScores()
+//    {
+//        s_Entries.Clear();
 
-    private static void SaveScores()
-    {
-        for (int i = 0; i < EntryCount; ++i)
-        {
-            var entry = s_Entries[i];
-            PlayerPrefs.SetString(PlayerPrefsBaseKey + "[" + i + "].name", entry.name);
-            PlayerPrefs.SetInt(PlayerPrefsBaseKey + "[" + i + "].score", entry.score);
-        }
-    }
+//        for (int i = 0; i < EntryCount; ++i)
+//        {
+//            ScoreEntry entry;
+//            entry.name = PlayerPrefs.GetString(PlayerPrefsBaseKey + "[" + i + "].name", "");
+//            entry.score = PlayerPrefs.GetInt(PlayerPrefsBaseKey + "[" + i + "].playerTotalScore", 0);
+//            s_Entries.Add(entry);
+//        }
 
-    public static ScoreEntry GetEntry(int index)
-    {
-        return Entries[index];
-    }
+//        SortScores();
+//    }
 
-    public static void Record(string name, int score)
-    {
-        Entries.Add(new ScoreEntry(name, score));
-        SortScores();
-        Entries.RemoveAt(Entries.Count - 1);
-        SaveScores();
-    }
-}
+//    private static void SaveScores()
+//    {
+//        for (int i = 0; i < EntryCount; ++i)
+//        {
+//            var entry = s_Entries[i];
+//            PlayerPrefs.SetString(PlayerPrefsBaseKey + "[" + i + "].name", entry.name);
+//            PlayerPrefs.SetInt(PlayerPrefsBaseKey + "[" + i + "].score", entry.score);
+//        }
+//    }
+
+//    public static ScoreEntry GetEntry(int index)
+//    {
+//        return Entries[index];
+//    }
+
+//    public static void Record(string name, int score)
+//    {
+//        Entries.Add(new ScoreEntry(name, score));
+//        SortScores();
+//        Entries.RemoveAt(Entries.Count - 1);
+//        SaveScores();
+//    }
+//}
 
---
+++ 7WeekGame/Assets/Scripts/MenuScripts/RobLeaderboard.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RobLeaderboard : MonoBehaviour {
+
+    public static int score;
+
+    public static int highscore;
+
+    public GUIText text;
+
+    public InputField type;
+
+    // Use this for initialization
+    void Start ()
+    {
+
+        text = GetComponent<GUIText>();
+
+        text.text = "";
+
+       // PlayerPrefs.SetInt("highscore", highscore);
+
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+      
+
+	}
+}
